f5ef7f0f925385f0f86160ac17ee159b721e322f
David Mazza <github@dmzza.com>
2014-01-20 01:59:50 -0500

    Initial Commit

---
+++ Paths/PKDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  PKDetailViewController.h
+//  Paths
+//
+//  Created by David Mazza on 1/20/14.
+//  Copyright (c) 2014 Peaking Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKDetailViewController : UIViewController <UISplitViewControllerDelegate>
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
