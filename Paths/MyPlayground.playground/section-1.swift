b3062cc0769eb13fa2944522d48a165b2a3dcf18
David Mazza <github@dmzza.com>
2014-06-03 22:50:10 -0700

    First swift playground

---
+++ Paths/MyPlayground.playground/section-1.swift
@@ -0,0 +1,19 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+println("Hello world")
+var num = 1
+num++
+num
+while num < 10 {
+    num++
+    println(num)
+}
+num
+
+
+
+
+
