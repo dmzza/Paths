07e6921177ad7b63794c08313f3aea2d2dd0ff62
David Mazza <github@dmzza.com>
2014-07-30 17:17:49 -0700

    Animation is glitchy

--- Shot Rocket/PKDetailViewController.m
+++ Shot Rocket/PKDetailViewController.m
@@ -60,6 +60,7 @@
 - (IBAction)didDragShot:(UIPanGestureRecognizer *)sender {
     CGPoint translation = [sender translationInView:self.view];
     UIGestureRecognizerState state = [sender state];
+    __weak __typeof__(self) weakSelf = self;
     
     switch (state) {
         case UIGestureRecognizerStateBegan:
@@ -70,6 +71,43 @@
             [self.shotView setTransform:CGAffineTransformMakeTranslation(translation.x*2, translation.y*2)];
             break;
         
+        case UIGestureRecognizerStateEnded:
+            [self.shotView setTransform:CGAffineTransformMakeTranslation(translation.x*2, translation.y*2)];
+            if (translation.x > 0) {
+                [UIView animateWithDuration:0.5 animations:^{
+                    [weakSelf.shotView setTransform:CGAffineTransformMakeTranslation(640, translation.y)];
+                } completion:^(BOOL finished) {
+                    [weakSelf likeShot];
+                    [weakSelf.shotView setTransform:CGAffineTransformIdentity];
+                }];
+                
+                /*[UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:1.0 initialSpringVelocity:10 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+                    [weakSelf.shotView setTransform:CGAffineTransformMakeTranslation(640, translation.y)];
+                } completion:^(BOOL finished) {
+                    [weakSelf.shotView setTransform:CGAffineTransformMakeTranslation(-640, 0)];
+                    [weakSelf likeShot];
+                    [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:1.0 initialSpringVelocity:10 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+                        [weakSelf.shotView setTransform:CGAffineTransformIdentity];
+                    } completion:^(BOOL finished) {
+                        
+                    }];
+                }];*/
+            } else if (translation.x < 0) {
+                [UIView animateWithDuration:0.5 animations:^{
+                    [weakSelf.shotView setTransform:CGAffineTransformMakeTranslation(-640 - translation.x, 0)];
+                } completion:^(BOOL finished) {
+                    [weakSelf likeShot];
+                    [weakSelf.shotView setTransform:CGAffineTransformIdentity];
+                }];
+            } else {
+                [UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:1.0 initialSpringVelocity:10 options:0 animations:^{
+                    [self.shotView setTransform:CGAffineTransformIdentity];
+                } completion:^(BOOL finished) {
+                    
+                }];
+            }
+            
+            break;
         
         default:
             break;
