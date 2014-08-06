38336d4d8a04b3bc215741f3e361f3fb4c55e082
David Mazza <github@dmzza.com>
2014-08-05 19:09:30 -0700

    Cleaned up camera roll from VC

--- Shot Rocket/PKMasterViewController.h
+++ Shot Rocket/PKMasterViewController.h
@@ -10,15 +10,11 @@
 #import <AssetsLibrary/AssetsLibrary.h>
 #import <CoreData/CoreData.h>
 
-@class PKCameraRoll;
-@protocol PKCameraRollDelegate;
-
-@interface PKMasterViewController : UITableViewController <NSFetchedResultsControllerDelegate, PKCameraRollDelegate>
+@interface PKMasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
 
 @property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
 @property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
 @property (strong, nonatomic) ALAssetsLibrary *library;
-@property (strong, nonatomic) PKCameraRoll *cameraRoll;
 
 - (void)shotsDidFinishLoading;
 
