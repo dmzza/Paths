49642c3e5bfdbc33ca006f87e439810f3ac5cde2
David Mazza <github@dmzza.com>
2014-09-08 23:25:39 -0700

    Gradient over master VC cells

---
+++ Shot Rocket/PKGradientView.h
@@ -0,0 +1,13 @@
+//
+//  PKGradientView.h
+//  Shot Rocket
+//
+//  Created by David Mazza on 9/8/14.
+//  Copyright (c) 2014 Peaking Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PKGradientView : UIView
+
+@end
---
+++ Shot Rocket/PKGradientView.m
@@ -0,0 +1,44 @@
+//
+//  PKGradientView.m
+//  Shot Rocket
+//
+//  Created by David Mazza on 9/8/14.
+//  Copyright (c) 2014 Peaking Software LLC. All rights reserved.
+//
+
+#import "PKGradientView.h"
+
+@implementation PKGradientView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef currentContext = UIGraphicsGetCurrentContext();
+    
+    CGGradientRef glossGradient;
+    CGColorSpaceRef rgbColorspace;
+    size_t num_locations = 2;
+    CGFloat locations[2] = { 0.0, 1.0 };
+    CGFloat components[8] = { 1.0, 1.0, 1.0, 0.35,  // Start color
+        1.0, 1.0, 1.0, 0.06 }; // End color
+    
+    rgbColorspace = CGColorSpaceCreateDeviceRGB();
+    glossGradient = CGGradientCreateWithColorComponents(rgbColorspace, components, locations, num_locations);
+    
+    CGRect currentBounds = self.bounds;
+    CGPoint topCenter = CGPointMake(CGRectGetMidX(currentBounds), 0.0f);
+    CGPoint midCenter = CGPointMake(CGRectGetMidX(currentBounds), CGRectGetMidY(currentBounds));
+    CGContextDrawLinearGradient(currentContext, glossGradient, topCenter, midCenter, 0);
+    
+    CGGradientRelease(glossGradient);
+    CGColorSpaceRelease(rgbColorspace);
+}
+@end
